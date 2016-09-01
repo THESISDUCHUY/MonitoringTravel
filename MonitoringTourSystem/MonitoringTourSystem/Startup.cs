87bbcc8329ccb50b3234faa7ea37755bae03d79d
Duc Nguyen <nvduc2910@gmail.com>
2016-09-02 00:20:04 +0700

    web: Create project ASP.NET MVC

---
+++ MonitoringTourSystem/MonitoringTourSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonitoringTourSystem.Startup))]
+namespace MonitoringTourSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
